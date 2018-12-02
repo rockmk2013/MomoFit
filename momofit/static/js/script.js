61a4b26ce53eae911bdfae6a4e62436c4f796c0e
jui-feng tsai <michael05430543@gmail.com>
2018-12-02 21:47:42 +0800

    add date pickers

--- momofit/static/js/script.js
+++ momofit/static/js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
     $('.modal').modal();
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
