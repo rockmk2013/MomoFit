8a8365a320cedf68ebb8a88a24a851f036164e07
michelle12369 <p5i7gaaaa@gmail.com>
2018-12-07 02:56:21 +0800

    modify ui & clean code

--- momofit/static/js/script.js
+++ momofit/static/js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
   $('select').formSelect();
   $('.modal').modal();
   $('.sidenav').sidenav();
+  $('.tabs').tabs();
   var options = {format:'yyyy-mm-dd', setDefaultDate:true};
   $('.datepicker').datepicker(options);
 });
